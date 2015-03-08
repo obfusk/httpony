fec38d9f2c03cfacd941cec86076fe5438927546
Felix C. Stegerman <flx@obfusk.net>
2015-03-08 01:45:03 +0100

    ...

--- test/httpony_spec/server_spec.py
+++ test/httpony_spec/server_spec.py
@@ -2,13 +2,14 @@
 #
 # File        : server_spec.py
 # Maintainer  : Felix C. Stegerman <flx@obfusk.net>
-# Date        : 2015-03-01
+# Date        : 2015-03-07
 #
 # Copyright   : Copyright (C) 2015  Felix C. Stegerman
 # Licence     : LGPLv3+
 #
 # --                                                            ; }}}1
 
+import httpony.server as S
 import unittest
 
 # ...
